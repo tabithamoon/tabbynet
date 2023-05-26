ff94720c1c674fc1c39aa992b421d087d44604b8
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2023-05-26 09:45:01 -0300

    the first inklings of responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <title>the tabbynet</title>
 
-<div class="flex flex-row mx-auto w-min py-64">
-    <div class="flex flex-col min-h-full justify-center">
+<div class="flex flex-col xl:flex-row mx-auto w-min py-64">
+    <div class="flex flex-col min-h-full text-center xl:text-left justify-center text-6xl xl:text-9xl">
         <div>
-            <p class="text-9xl">Hi,</p>
-            <p class="text-9xl w-max text-orange-500">I'm Tabitha</p>
+            <p>Hi,</p>
+            <p class="xl:w-max text-orange-500">I'm Tabitha</p>
         </div>
         <div>
             <p class="text-4xl p-2">I'm a red panda on the internet.</p>
@@ -13,8 +13,4 @@
     <div class="p-8">
         <img class="max-w-lg" src="https://files.tabbynet.com/tabbynet/wah.jpg" alt="the wah!"/>
     </div>
-</div>
-
-<div class="mx-auto">
-    I'm not a web dev. Please forgive my sins.
 </div>
