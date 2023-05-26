76384cb6ac2907a3f44935af4b8274a474266093
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2023-05-26 07:43:00 -0300

    Work starts on new homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,20 @@
 <title>the tabbynet</title>
 
-<div class="flex flex-col text-center">
-    <h1 class="text-2xl p-2">welcome to the tabbynet</h1>
-    <div class="flex flex-col items-center">
-        <img class="max-w-lg" src="https://files.tabbynet.com/tabbynet/wah.jpg" alt="the wah!"/>
-        <p class="p-1">pictured: the wah</p>
+<div class="flex flex-row mx-auto w-min py-64">
+    <div class="flex flex-col min-h-full justify-center">
+        <div>
+            <p class="text-9xl">Hi,</p>
+            <p class="text-9xl w-max text-orange-500">I'm Tabitha</p>
+        </div>
+        <div>
+            <p class="text-4xl p-2">I'm a red panda on the internet.</p>
+        </div>
     </div>
-    
-    actual website coming soon i promise
-    <div class="p-10">made with SvelteKit and Tailwind<br /> (though you can't really tell right now)</div>
+    <div class="p-8">
+        <img class="max-w-lg" src="https://files.tabbynet.com/tabbynet/wah.jpg" alt="the wah!"/>
+    </div>
+</div>
+
+<div class="mx-auto">
+    I'm not a web dev. Please forgive my sins.
 </div>
