35645c85d569bd0284900a2ac3f96b68dc7460c2
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2023-05-26 10:09:52 -0300

    Added svelte-icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,20 @@
 <title>the tabbynet</title>
 
-<div class="flex flex-col xl:flex-row mx-auto w-min py-64">
+<div class="flex flex-col xl:flex-row items-center justify-center w-full h-max py-12 xl:h-screen">
     <div class="flex flex-col min-h-full text-center xl:text-left justify-center text-6xl xl:text-9xl">
         <div>
             <p>Hi,</p>
             <p class="xl:w-max text-orange-500">I'm Tabitha</p>
         </div>
         <div>
-            <p class="text-4xl p-2">I'm a red panda on the internet.</p>
+            <p class="text-2xl xl:text-4xl p-2">I'm a red panda on the internet.</p>
         </div>
     </div>
     <div class="p-8">
-        <img class="max-w-lg" src="https://files.tabbynet.com/tabbynet/wah.jpg" alt="the wah!"/>
+        <img class="xl:max-w-lg" src="https://files.tabbynet.com/tabbynet/wah.jpg" alt="the wah!"/>
     </div>
+</div>
+
+<div class="w-full text-center">
+    <p>I make software and memes.</p>
 </div>
